446e1d412bb77e6e3d1ff13b14e259cb383bc1fe
Bhin <bbhinhb@gmail.com>
2025-04-18 22:45:37 +0900

    feat: 저장
    FDORA-4

--- src/main/java/com/farmdora/farmdoraactivity/user/repository/UserDashboardRepository.java
+++ src/main/java/com/farmdora/farmdoraactivity/user/repository/UserDashboardRepository.java
@@ -6,14 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UserDashboardRepository extends JpaRepository<User, Integer> {
 
-    @Query("SELECT u.name, u.phoneNum, u.email " +
-            "FROM User u " +
-            "WHERE u.id = :userId")
-    Optional<User> findUserInfo(@Param("userId") Integer userId);
+    @Query("SELECT SUM(oo.price) FROM OrderOption oo JOIN oo.order o WHERE o.user.userId = :userId")
+    Long sumTotalAmount(@Param("userId") Integer userId);
+
+    @Query("SELECT COUNT(r.id) FROM Review r JOIN r.order o WHERE o.user.userId = :userId")
+    Long countReviewsByUserId(@Param("userId") Integer userId);
+
+    @Query("SELECT COUNT(q) FROM Question q WHERE q.user.userId = :userId")
+    Long countQuestionsByUserId(@Param("userId") Integer userId);
 }
