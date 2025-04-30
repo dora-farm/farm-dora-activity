2cf36f04ba029d6e5f8cb899cf7e2a144535565f
Bhin <bbhinhb@gmail.com>
2025-04-30 17:04:35 +0900

    Refactor: 불필요한 코드 제거
    FDORA-12

--- src/main/java/com/farmdora/farmdoraactivity/seller/controller/OrderManageController.java
+++ src/main/java/com/farmdora/farmdoraactivity/seller/controller/OrderManageController.java
@@ -2,7 +2,6 @@ package com.farmdora.farmdoraactivity.seller.controller;
 
 import com.farmdora.farmdoraactivity.common.response.HttpResponse;
 import com.farmdora.farmdoraactivity.seller.service.OrderManageService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.farmdora.farmdoraactivity.common.response.SuccessMessage.SEARCH_ORDERMANAGECOUNT_SUCCESS;
-import static com.farmdora.farmdoraactivity.common.response.SuccessMessage.SEARCH_ORDERMANAGELIST_SUCCESS;
 
 @RestController
 @RequestMapping("/api/my/seller/order")
